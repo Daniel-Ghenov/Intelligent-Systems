bf055d0f4e63edb88b8eafc18bee3691592ed7a1
Daniel Ghenov <daniel.ghenov@protonmail.com>
2025-11-16 22:00:58 +0200

    HW 4

--- Homeworks/HW 4/tsp.cpp
+++ Homeworks/HW 4/tsp.cpp
@@ -39,21 +39,21 @@ class TSPGeneticSolver
 {
 private:
     std::vector<std::pair<double, double>> points;
+    std::vector<std::vector<double>> distanceMatrix;
     std::vector<Path> population;
+    std::mt19937 rng{std::random_device{}()};
     int populationSize = 100;
     static const int POPULATION_KEEP = 2;
-    static const int POPULATION_KEEP_RESTART_PERCENTAGE = 20;
-    static const int MAX_GENERATIONS_COEF = 500;
+    static const int POPULATION_KEEP_RESTART_PERCENTAGE = 2;
+    static const int MAX_GENERATIONS = 10000;
     static const int STAGNATION_COEF = 2;
-    static const int MIN_POPULATION_SIZE = 100;
-    static const int MAX_POPULATION_SIZE = 1500;
     static const int GENERATIONS_TO_PRINT = 10;
-    static const int MUTATION_PERCENTAGE = 40;
+    static const int MUTATION_PERCENTAGE = 5;
 public:
     explicit TSPGeneticSolver(const std::vector<std::pair<double, double>>& points)
         : points(points)
     {
-        populationSize = std::max(MIN_POPULATION_SIZE, std::min(MAX_POPULATION_SIZE, static_cast<int>(10 * std::sqrt(points.size()))));
+        distanceMatrix = getDistanceMatrix(points);
         addRandomPopulation(populationSize);
     }
 
@@ -63,7 +63,7 @@ public:
         {
             return bestPathBaseCase();
         }
-        int maxGenerations = MAX_GENERATIONS_COEF * points.size();
+        int maxGenerations = MAX_GENERATIONS;
         int printGenerationInterval = maxGenerations / GENERATIONS_TO_PRINT;
         double lastBestWeight = population[0].totalWeight;
         int stagnationCounter = 0;
@@ -71,15 +71,14 @@ public:
         for (int generation = 0; generation < maxGenerations; generation++)
         {
             std::vector<Path> newPopulation;
-            std::vector<Path> parents = selectParents();
-            std::vector<Path> children = makePairs(parents);
+            std::vector<Path> children = makeChildren(population.size());
 
             newPopulation.insert(newPopulation.end(), children.begin(), children.end());
             mutate(newPopulation);
             newPopulation.insert(newPopulation.end(), population.begin(), population.begin() + POPULATION_KEEP);
             std::sort(newPopulation.begin(), newPopulation.end());
             newPopulation.resize(populationSize);
-            population = newPopulation;
+            population = std::move(newPopulation);
 
             if (population[0].totalWeight < lastBestWeight)
             {
@@ -95,7 +94,7 @@ public:
                     stagnationCounter = 0;
                 }
             }
-            if (shouldPrint && generation % printGenerationInterval == 0)
+            if (shouldPrint && (generation % printGenerationInterval == 0 || generation == maxGenerations - 1))
             {
                 std::cout << population[0].totalWeight << std::endl;
             }
@@ -109,16 +108,6 @@ private:
     {
         for (auto & path : populationToMutate)
         {
-            bool shouldMutateRev = (rand() % 100) < MUTATION_PERCENTAGE;
-            if (shouldMutateRev)
-            {
-                int size = path.nodes.size();
-                int index1 = rand() % size;
-                int index2 = rand() % size;
-                std::reverse(path.nodes.begin() + std::min(index1, index2),
-                             path.nodes.begin() + std::max(index1, index2) + 1);
-                path.totalWeight = getPathWeight(path.nodes);
-            }
             bool shouldMutateSwap = (rand() % 100) < MUTATION_PERCENTAGE;
             if (shouldMutateSwap)
             {
@@ -135,146 +124,74 @@ private:
         }
     }
 
-    std::vector<Path> selectParents()
+    Path tournamentSelect(const std::vector<Path>& candidates) const
     {
-//        //rank based selection
-//        int totalRank = (populationSize * (populationSize + 1)) / 2;
-//        std::vector<Path> selectedParents;
-//        std::vector<double> selectionProbabilities(populationSize);
-//        for (int i = 0; i < populationSize; i++)
-//        {
-//            selectionProbabilities[i] = static_cast<double>(populationSize - i) / totalRank;
-//        }
-//        std::discrete_distribution<int> distribution(selectionProbabilities.begin(), selectionProbabilities.end());
-//        std::mt19937 gen(std::random_device{}());
-//        for (int i = 0; i < populationSize; i++)
-//        {
-//            int parentIndex = distribution(gen);
-//            selectedParents.push_back(population[parentIndex]);
-//        }
-//        return selectedParents;
-
-        //tournament based selection
-        //k = 3
         int tournamentSize = 3;
-        std::vector<Path> selectedParents;
-        for (int i = 0; i < populationSize; i++)
+        int bestIdx = rand() % populationSize;
+        for (int j = 1; j < tournamentSize - 1; j++)
         {
-            int bestIdx = rand() % populationSize;
-            for (int j = 1; j < tournamentSize - 1; j++)
+            int currIdx = rand() % populationSize;
+            if (currIdx < bestIdx)
             {
-                int currIdx = rand() % populationSize;
-                if (currIdx < bestIdx)
-                {
-                    bestIdx = currIdx;
-                }
+                bestIdx = currIdx;
             }
-            selectedParents.push_back(population[bestIdx]);
         }
-        return selectedParents;
+        return candidates[bestIdx];
     }
 
-    std::vector<Path> makePairs(const std::vector<Path> &parents)
+    std::vector<Path> makeChildren(int childrenCount)
     {
         std::vector<Path> children;
+        children.resize(childrenCount);
 
-        for(int i = 0; i < parents.size(); i+=2)
+        for(int i = 0; i < childrenCount; i++)
         {
-            std::pair<Path, Path> currChildren = makePair(parents[i], parents[i+1]);
-            children.push_back(currChildren.first);
-            children.push_back(currChildren.second);
+            Path parent1 = tournamentSelect(population);
+            Path parent2 = tournamentSelect(population);
+            Path child = makeChild(parent1, parent2);
+            children[i] = child;
         }
         return children;
     }
-    std::pair<Path, Path> makePair(const Path& parent1, const Path& parent2)
+
+    Path makeChild(const Path& parent1, const Path& parent2)
     {
         int size = parent1.nodes.size();
-        int i = rand() % (parent1.nodes.size() - 2);
-        int j = rand() % (parent1.nodes.size() - 2);
-        int k;
+        int i = rand() % parent1.nodes.size();
+        int j = rand() % parent1.nodes.size();
+        while (i == j)
+        {
+            j = rand() % parent1.nodes.size();
+        }
         if (i > j) std::swap(i, j);
-        i += 1;
-        j += 1;
         std::vector<int> child1Nodes(parent1.nodes.size(), -1);
-        std::vector<int> child2Nodes(parent2.nodes.size(), -1);
 
-        std::unordered_map<int, int> parent1Positions;
-        std::unordered_map<int, int> parent2Positions;
+        std::vector<int> parent2Positions(size);
         for (int index = 0; index < size; index++)
         {
-            parent1Positions[parent1.nodes[index]] = index;
             parent2Positions[parent2.nodes[index]] = index;
         }
 
-//        std::unordered_set<int> child1Set;
-//        std::unordered_set<int> child2Set;
+        std::vector<bool> child1Set(size, false);
         for (int k = i; k <= j; k++)
         {
             child1Nodes[k] = parent1.nodes[k];
-            child2Nodes[k] = parent2.nodes[k];
-//            child1Set.insert(parent1.nodes[k]);
-//            child2Set.insert(parent2.nodes[k]);
+            child1Set[parent1.nodes[k]] = true;
         }
 
-        for (int k = i; k <= j; k++)
+        int currentIndex1 = (j + 1) % size;
+
+        for (int k = 0; k < size; k++)
         {
-            int parent2Node = parent2.nodes[k];
-            if (parent1Positions[parent2Node] < i || parent1Positions[parent2Node] > j)
+            int parent2Node = parent2.nodes[(j + 1 + k) % size];
+            if (!child1Set[parent2Node])
             {
-                int currentIndex = k;
-                while(currentIndex >= i && currentIndex <= j)
-                {
-                    int currentNodeP1 = parent1.nodes[currentIndex];
-                    currentIndex = parent2Positions[currentNodeP1];
-                }
-                child1Nodes[currentIndex] = parent2Node;
-            }
-            int parent1Node = parent1.nodes[k];
-            if (parent2Positions[parent1Node] < i || parent2Positions[parent1Node] > j)
-            {
-                int currentIndex = k;
-                while(currentIndex >= i && currentIndex <= j)
-                {
-                    int currentNodeP2 = parent2.nodes[currentIndex];
-                    currentIndex = parent1Positions[currentNodeP2];
-                }
-                child2Nodes[currentIndex] = parent1Node;
+                child1Nodes[currentIndex1] = parent2Node;
+                currentIndex1 = (currentIndex1 + 1) % size;
             }
         }
-
-        for (k = 0; k < size; k++)
-        {
-            if (child1Nodes[k] == -1)
-            {
-                child1Nodes[k] = parent2.nodes[k];
-            }
-            if (child2Nodes[k] == -1)
-            {
-                child2Nodes[k] = parent1.nodes[k];
-            }
-        }
-
-//        int currentIndex1 = (j + 1) % size;
-//        int currentIndex2 = (j + 1) % size;
-//
-//        for (int k = 0; k < size; k++)
-//        {
-//            int parent2Node = parent2.nodes[(j + 1 + k) % size];
-//            if (child1Set.find(parent2Node) == child1Set.end())
-//            {
-//                child1Nodes[currentIndex1] = parent2Node;
-//                currentIndex1 = (currentIndex1 + 1) % size;
-//            }
-//            int parent1Node = parent1.nodes[(j + 1 + k) % size];
-//            if (child2Set.find(parent1Node) == child2Set.end())
-//            {
-//                child2Nodes[currentIndex2] = parent1Node;
-//                currentIndex2 = (currentIndex2 + 1) % size;
-//            }
-//        }
         double child1Weight = getPathWeight(child1Nodes);
-        double child2Weight = getPathWeight(child2Nodes);
-        return {Path(child1Nodes, child1Weight), Path(child2Nodes, child2Weight)};
+        return {child1Nodes, child1Weight};
     }
 
     void addRandomPopulation(int randomCount)
@@ -292,7 +209,7 @@ private:
 
         for (int i = 0; i < randomCount; i++)
         {
-            std::shuffle(nodes.begin(), nodes.end(), std::mt19937(std::random_device()()));
+            std::shuffle(nodes.begin(), nodes.end(), rng);
             population.emplace_back(nodes, getPathWeight(nodes));
         }
 
@@ -306,8 +223,8 @@ private:
         {
             int from = nodes[j];
             int to = nodes[j + 1];
-            totalWeight += calculateDistance(points[from].first, points[from].second,
-                                             points[to].first, points[to].second);
+
+            totalWeight += distanceMatrix[from][to];
         }
         return totalWeight;
     }
@@ -327,14 +244,29 @@ private:
                                           points[1].first, points[1].second);
         return Path({0, 1}, weight);
     }
+
+    std::vector<std::vector<double>> getDistanceMatrix(const std::vector<std::pair<double, double>> &vector)
+    {
+        int n = vector.size();
+        std::vector<std::vector<double>> matrix(n, std::vector<double>(n, 0.0));
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                double dist = calculateDistance(vector[i].first, vector[i].second,
+                                                vector[j].first, vector[j].second);
+                matrix[i][j] = dist;
+                matrix[j][i] = dist;
+            }
+        }
+        return matrix;
+    }
 };
 
 const int TSPGeneticSolver::POPULATION_KEEP;
 const int TSPGeneticSolver::POPULATION_KEEP_RESTART_PERCENTAGE;
-const int TSPGeneticSolver::MAX_GENERATIONS_COEF;
+const int TSPGeneticSolver::MAX_GENERATIONS;
 const int TSPGeneticSolver::STAGNATION_COEF;
-const int TSPGeneticSolver::MIN_POPULATION_SIZE;
-const int TSPGeneticSolver::MAX_POPULATION_SIZE;
 const int TSPGeneticSolver::GENERATIONS_TO_PRINT;
 const int TSPGeneticSolver::MUTATION_PERCENTAGE;
 
